a838d55090f2659d5c0c891bacdae0909eff64e4
0verseerPepek <1killcube1@gmail.com>
2019-06-12 22:49:22 +0300

    tests for user_param_controller

--- spec/controllers/user_param_controller_spec.rb
+++ spec/controllers/user_param_controller_spec.rb
@@ -3,50 +3,81 @@ require 'rails_helper'
 RSpec.describe Api::V1::UserParamsController, type: :request do
     let!(:user) {FactoryBot.create(:user)}
     let!(:user_param) {user.build_user_param(first_name: "tester", last_name: "testerow", 
-                  email: "test@gmail.com", address: "Ungvar s. Radvanka 5") }
-               #before(:each) do
-              #   @current_user = FactoryBot.create(:user)
-                #end                 
+                       email: "test@gmail.com", address: "Ungvar s. Radvanka 5").save}              
 
     describe "POST user_param#create" do 
      it "should create user_param" do
-        login(user)
+        sign_up(user)
         auth_params = get_auth_params_from_login_response_headers(response)
-        post '/api/v1/user/params', params: {user_param: nil}, headers: auth_params
-        pp json = JSON.parse(response.body)
+        post '/api/v1/user/params', params: {first_name: "tester", last_name: "testerow", 
+                                             email: "qasad@gmail.com", address: "Ungvar s. Radvanka 5"}, 
+                                             headers: auth_params
+        json = JSON.parse(response.body)
         expect(json["success"]).to eq(true)
         expect(response.status).to eq(201)
         end
-     it "should render error, user already exist" do
+
+     it "should render error, user_param already exist" do
           login(user)
           headers = get_auth_params_from_login_response_headers(response)
-          post '/api/v1/user/params', params: {current_user: user, user_param: user.user_param}, headers: headers
-          pp json = JSON.parse(response.body)
-          expect(json["msg"]).to eq("user params already exist")
+          post '/api/v1/user/params', params: {first_name: "tester1", last_name: "testerow2", 
+                                      email: "qasad@gmail.com", address: "Ungvar s. Radvanka 5"}, 
+                                      headers: headers
+          json = JSON.parse(response.body)
+          expect(response.status).to eq(400)
+          expect(json["success"]).to eq(false)
+          expect(json["msg"]).to eq(['user params already exist'])
         end  
-     
-      
+      end
 
-        def get_auth_params_from_login_response_headers(response)
-            client = response.headers["client"]
-            token = response.headers['access-token']
-            expiry = response.headers['expiry']
-            token_type = response.headers['token-type']  
-            uid =   response.headers['uid']  
-      
-            auth_params = {
-                            'access-token' => token,
-                            'client' => client,
-                            'uid' => uid,
-                            'expiry' => expiry,
-                            'token_type' => token_type
-                          }
-            auth_params
-        end
+    describe "GET user_param#show" do
+      it "should show user_param" do
+        login(user)
+        headers = get_auth_params_from_login_response_headers(response)
+        get '/api/v1/user/params', params: nil, headers: headers
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json["success"]).to eq(true)
+        expect(json["user_param"]["first_name"]).to eq("tester")
+        expect(json["user_param"]["email"]).to eq("test@gmail.com")
+      end
+
+      it "should render error, params doesn't exist" do 
+        sign_up(user)
+        headers = get_auth_params_from_login_response_headers(response)
+        get '/api/v1/user/params', params: nil, headers: headers
+        expect(response.status).to eq(400)
+        json = JSON.parse(response.body)
+        expect(json["success"]).to eq(false)
+        expect(json["msg"]).to eq(["user params doesn't exist"])
+      end
     end
-    def login(user)
-      post '/auth/sign_in', params:  { email: "test@gmail.com", password: "pass_123"}.to_json,
-                            headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
-    end 
-  
+
+    describe "PUT user_param#update" do
+      it "should update user_param" do 
+        login(user)
+        headers = get_auth_params_from_login_response_headers(response)
+        put '/api/v1/user/params', params: {first_name: "tester_upd", last_name: "testerow_upd", 
+                                            email: "qasad@gmail.com_upd", address: "Ungvar s. Radvanka 5 _upd"}, 
+                                            headers: headers
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json["user_param"]["first_name"]).to eq("tester_upd")
+        expect(json["user_param"]["last_name"]).to eq("testerow_upd")
+        expect(json["user_param"]["email"]).to eq("qasad@gmail.com_upd")
+        expect(json["user_param"]["address"]).to eq("Ungvar s. Radvanka 5 _upd")
+      end
+
+      it "should render error, params doesn't exist" do 
+        sign_up(user)
+        headers = get_auth_params_from_login_response_headers(response)
+        put '/api/v1/user/params', params: {first_name: "tester_upd", last_name: "testerow_upd", 
+                                            email: "qasad@gmail.com_upd", address: "Ungvar s. Radvanka 5 _upd"}, 
+                                            headers: headers
+        expect(response.status).to eq(400)
+        json = JSON.parse(response.body)
+        expect(json["success"]).to eq(false)
+        expect(json["msg"]).to eq(["user params doesn't exist"])
+      end
+    end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,17 +1,49 @@
 module ControllerMacros
-  def login_admin
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in FactoryBot.create(:admin) # Using factory girl as an example
-    end
+  def get_auth_params_from_login_response_headers(response)
+    client = response.headers["client"]
+    token = response.headers['access-token']
+    expiry = response.headers['expiry']
+    token_type = response.headers['token-type']  
+    uid =   response.headers['uid']  
+    content_type = response.headers['content-type']
+
+    auth_params = {
+                    'access-token' => token,
+                    'client' => client,
+                    'uid' => uid,
+                    'expiry' => expiry,
+                    'token_type' => token_type,
+                    'content_type' => content_type
+                  }
+    auth_params
   end
 
-  def login_user
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryBot.create(:user)
-      #user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-      sign_in user
-    end
+  def create_auth_header_from_scratch(user1)
+    @current_user = user1      
+    client_id = SecureRandom.urlsafe_base64(nil, false)
+    token     = SecureRandom.urlsafe_base64(nil, false)
+
+    @current_user.tokens[client_id] = {
+    token: BCrypt::Password.create(token),
+    expiry: (Time.now + 1.day).to_i
+    }
+    new_auth_header = @current_user.build_auth_header(token, client_id)
+
   end
+
+
+  def login(user)
+    post '/auth/sign_in', params:  { email: user.email, password: user.password}.to_json,
+                    headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+  end 
+
+  def sign_up(user)
+
+    females = ["elizabeth", "rose", "juliet"]
+    surnameFemales = ["rosewelth", "gates", "jobs"]
+    providers = ["gmail.com", "hotmail.com", "yahoo.com", "mail.ru"]
+    
+    post '/auth', params:  { email: "#{females.sample}.#{surnameFemales.sample}#{rand(252...4350)}@#{providers.sample}", password: user.password}.to_json,
+                    headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+  end 
 end
